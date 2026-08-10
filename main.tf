511ba207260e907cf236a1fade65695d7baf8795
pramodcloud <pmslmaurya@gmail.com>
2026-08-10 21:40:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "demo-rg"
+  name     = "demo-rg11"
   location = "Central India"
 }
 resource "azurerm_resource_group" "rg2" {
-  name     = "demo-rg1"
+  name     = "demo-rg211"
   location = "Central India"
 }
 resource "azurerm_resource_group" "rg3" {
-  name     = "demo-rg2"
+  name     = "demo-rg123"
   location = "Central India"
 }
